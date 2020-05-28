4bf2ae5f786d8029f26c77b6dc2c988b432ab6bc
梅林的法杖 <530264127@qq.com>
2020-05-28 14:55:44 +0800

    first init =

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  dest: 'dist',
+  title: '极简主义的生活方式',
+  themeConfig: {
+    editLinks: false,
+    docsDir: 'docs',
+    search: false,
+    lastUpdated: '更新时间',
+    nav: [],
+    sidebar: [
+      {
+        title: '极简衣橱',
+        path: '/cloth/',
+        collapsable: true,
+        sidebarDepth: 1,
+        children: []
+      }
+    ]
+  }
+}
